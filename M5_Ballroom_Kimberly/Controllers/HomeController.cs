d5b4cd23e1ca0902d474b572bae05cd435b6ea58
youneedanemail <youneedanemail@gmail.com>
2024-02-09 21:21:09 -0700

    Add project files.

---
+++ M5_Ballroom_Kimberly/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using M5_Ballroom_Kimberly.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace M5_Ballroom_Kimberly.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LearnToDance()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ M5_Ballroom_Kimberly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace M5_Ballroom_Kimberly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
